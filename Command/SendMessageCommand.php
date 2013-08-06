5ba368cf658339b990dc16bfb8c565090ba1cd6a
Pablo Godel <pgodel@gmail.com>
2013-08-06 12:02:22 -0400

    changed room from argument to option

--- Command/SendMessageCommand.php
+++ Command/SendMessageCommand.php
@@ -22,9 +22,9 @@ class SendMessageCommand extends ContainerAwareCommand
         $this
                 ->setName('hipchat:send:message')
                 ->setDescription('Sends a message to a HipChat room')
-                ->addArgument('room', InputArgument::REQUIRED, 'The HipChat Room')
                 ->addArgument('message', InputArgument::REQUIRED, 'The Message')
                 ->addArgument('from', InputArgument::OPTIONAL, 'Sender Name', 'HipchatBundle')
+                ->addOption('room', null, InputOption::VALUE_REQUIRED, 'The HipChat Room')
                 ->addOption('notify', null, InputOption::VALUE_NONE, 'Notify room members')
                 ->addOption('color', null, InputOption::VALUE_OPTIONAL, 'Color', 'yellow')
                 ->addOption('format', null, InputOption::VALUE_OPTIONAL, 'Message format', 'html');
@@ -36,15 +36,19 @@ class SendMessageCommand extends ContainerAwareCommand
         $this->input = $input;
         $this->output = $output;
 
-        $room = $input->getArgument('room');
         $message = $input->getArgument('message');
         $from = $input->getArgument('from');
 
+        $room = $input->getOption('room');
         $notify = $input->getOption('notify');
         $color = $input->getOption('color');
         $format = $input->getOption('format');
 
         $hipChat = $this->getContainer()->get('hipchat');
+
+        if (empty($room)) {
+            throw new \InvalidArgumentException("Please provide a valid room");
+        }
 
         $hipChat->message_room($room, $from, $message, $notify, $color, $format);
     }
